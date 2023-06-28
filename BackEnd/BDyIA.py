ae96c0135f901bcbec9ac21c7241626ae8de7103
AgusFuentes17 <fuentes.angelagustinet36@gmail.com>
2023-06-28 16:23:38 +0200

    añadir registros

--- BackEnd/BDyIA.py
+++ BackEnd/BDyIA.py
@@ -3,12 +3,28 @@ from pymongo.server_api import ServerApi
 
 uri = "mongodb+srv://PcMage:PcMage123@cluster1.lc6nmqa.mongodb.net/?retryWrites=true&w=majority"
 
-# Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
 
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+collection = informaciónclientes ["clientes_españa"]
+
+informaciónclientes = [
+{
+"nombre" : "García",
+"dirección" : "Calle Ejemplo 10",
+"código postal" : "46006",
+"ciudad" : "Valencia"
+}
+{
+"nombre" : "Rodríguez",
+"dirección" : "Calle Principal 1",
+"código postal" : "28007",
+"ciudad" : "Madrid"
+}
+{
+"nombre" : "González",
+"dirección" : "Calle Amplia 2",
+"código postal" : "36002",
+"ciudad" : "Pontevedra"
+}
+]
+collection.insert_many(informaciónclientes)
