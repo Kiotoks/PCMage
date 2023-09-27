23bbaa95532ec8b81fed4897b84c89e085ae5df3
GendoFR <gendofr@gmail.com>
2023-09-27 12:27:22 -0300

    traduccion de html a ejs

--- BackEnd/node/node_prueba.js
+++ BackEnd/node/node_prueba.js
@@ -8,10 +8,19 @@ app.set("views", __dirname + "/views");
 
   
 app.get("/", (req, res) => {
-    res.render("sobrenos", { titulo: "inicio EJS" });
+    res.render("index", { titulo: "inicio EJS" });
     //res.sendFile(__dirname + "/public/index.html"); funcional
 });
 
+app.get("/buscar", (req, res) => {
+    res.render("buscar", { titulo: "inicio EJS" });
+    //res.sendFile(__dirname + "/public/index.html"); funcional
+});
+
+app.get("/sobrenos", (req, res) => {
+    res.render("sobrenos", { titulo: "inicio EJS" });
+    //res.sendFile(__dirname + "/public/index.html"); funcional
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
